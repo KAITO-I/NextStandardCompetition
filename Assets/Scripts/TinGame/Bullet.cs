1be19f2ec67bc94098082a9cd7aef3e2b1c39b2b
tokisada_tnw <tokisada.tnw@gmail.com>
2020-02-13 15:27:30 +0900

    最終コミット

---
+++ Assets/Scripts/TinGame/Bullet.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [SerializeField]
+    private float angle = 0;
+    [SerializeField]
+    private float speed = 0;
+    [SerializeField]
+    private SpriteRenderer renderer;
+
+    private InstanceBulletList instanceBulletList;
+    private bool move = false;
+    //private int no = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.Rotate(0, 0, angle);
+    }
+
+    //初期化
+    public void EnabledInit(InstanceBulletList _instanceBulletList)
+    {
+        move = false;
+        transform.position = Vector3.zero;
+        gameObject.SetActive(false);
+        if (instanceBulletList == null)
+        {
+            instanceBulletList = _instanceBulletList;
+        }
+    }
+
+    public void DisEnabledInit()
+    {
+        move = true;
+        gameObject.SetActive(true);
+    }
+
+    private void Update()
+    {
+        if (!move) return;
+        if (!renderer.isVisible) instanceBulletList.Destroy();
+        transform.position += transform.up * speed * Time.deltaTime;
+    }
+}
---
+++ Assets/Scripts/TinGame/Warning.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Warning : MonoBehaviour
+{
+    [SerializeField]
+    private float displayTime = 0;
+    [SerializeField]
+    private float flashSpeed = 0;
+    [SerializeField]
+    private Image warningImage = null;
+
+    public bool IsStart { set; get; }
+
+    public IEnumerator WarningAnimation()
+    {
+        #region 初期化
+        float _timeCount = 0;
+        float _alpha = 0;
+        warningImage.gameObject.SetActive(true);
+        #endregion
+
+        while (_timeCount < displayTime)
+        {
+            _timeCount += Time.deltaTime;
+            #region 点滅処理
+            //alpha値の設定
+            _alpha = Mathf.Sin(Time.time * flashSpeed) / 2 + 0.5f;
+            //透明度の変更
+            warningImage.color = new Color(1f, 1f, 1f, _alpha);
+            #endregion
+            yield return null;
+        }
+
+        #region 終了処理
+        warningImage.gameObject.SetActive(false);
+        IsStart = true;
+        #endregion
+    }
+}　
