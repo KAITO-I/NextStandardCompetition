385dbdb2e76cb9d93f0167bbb8a98d9db313faa5
KAITO-I <inafuku.tnw@gmail.com>
2020-02-13 14:35:49 +0900

    ステージ選択スクリプト同封し忘れ

---
+++ Assets/Scripts/StageSelectManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class StageSelectManager : MonoBehaviour
+{
+    public void LoadGame01()
+    {
+        SceneLoader.LoadScene(SceneLoader.Scenes.Game01);
+    }
+
+    public void LoadGame02()
+    {
+        SceneLoader.LoadScene(SceneLoader.Scenes.Game02);
+    }
+
+    public void LoadGame03()
+    {
+        SceneLoader.LoadScene(SceneLoader.Scenes.Game03);
+    }
+}
