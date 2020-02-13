223d78d1cd0860ab3aabd4b27edd78fa1276cb49
SA371516 <baseball.syunta@gmail.com>
2020-02-13 15:34:09 +0900

    よろしく

--- Assets/Scrpits/HARAProject/CameraMove.cs
+++ Assets/Scrpits/HARAProject/CameraMove.cs
@@ -7,17 +7,19 @@ public class CameraMove : MonoBehaviour
     bool MoveChack;
     [SerializeField]
     float speed;
+    HaraManager _manager;
 
     // Start is called before the first frame update
     void Start()
     {
+        _manager = GameObject.FindWithTag("Manager").GetComponent<HaraManager>();
         MoveChack = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (MoveChack)//カメラが動いているかどうか
+        if (_manager._start)//カメラが動いているかどうか
         {
             gameObject.transform.position += new Vector3(speed * 1 * Time.deltaTime, 0, 0);
         }
--- Assets/Scrpits/HARAProject/HaraManager.cs
+++ Assets/Scrpits/HARAProject/HaraManager.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 public class HaraManager : MonoBehaviour
 {
     int _exCount;
+    [SerializeField]
     Text _clear;
+    float _time;
+    public bool _start;
     public int GetSetEx
     {
         get  { return _exCount; }
@@ -18,6 +21,20 @@ public class HaraManager : MonoBehaviour
     private void Start()
     {
         _clear.text = "";
+        _start = false;
+        _time = 3f;
+    }
+
+    private void Update()
+    {
+        _time -= Time.deltaTime;
+        string str = string.Format("{0:0}", _time);
+        _clear.text = str;
+        if (_time < 0)
+        {
+            _start = true;
+            _clear.text = "";
+        }
     }
 
     public void GameClear()
